8f236a5d8dfbb59549b9140576610a2ece97b426
ljpagno <ljpagno@gmail.com>
2012-10-02 22:36:31 -0300

    IguassuPass

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IguassuPass::Application.config.secret_token = 'd94a38e57a1864791197bba73b82239e2ec6e30f9a405a9897f5130bc6850873382f89316910d3265e07168bc5f91c905a87f2d1130ca69de8a7202ac3f9b7f7'
